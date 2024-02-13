445e1cb5ba7392192f4d4ddbfe31710008985697
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-13 15:30:07 -0500

    Got basic calculator operations to function.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,9 @@
 function add(x,y) {
-    return x + y;
+    return Number(x) + Number(y);
 };
 
 function subtract(x,y) {
-    return x - y;
+    return Number(x) - Number(y);
 };
 
 function multiply(x,y) {
@@ -37,23 +37,46 @@ function createButtons() {
         const button = document.createElement("button");
         button.id = i;
         button.textContent = i;
-        button.addEventListener("click", function() {calcInput.push(i)});
+        if (i === "=") {
+            button.addEventListener("click", operate);
+        }
+        else {
+            button.addEventListener("click", function() {calcInput.push(i)});
+        };
         operators.appendChild(button);
     });
 };
 
 function operate() {
-    const operator = calcInput.find((item) => item === ("+" || "-" || "×" || "÷"));
-    const calcString = calcInput.toString();
+    //console.log(calcInput);
+    const operator = calcInput.find((item) => item === "+" || item === "-" || item === "×" || item === "÷");
+    //console.log(operator);
+    const calcString = calcInput.join("");
+    //console.log(calcString);
+    const numArray = calcString.split(operator);
+    //console.log(numArray);
+    
+    let x = numArray[0];
+    let y = numArray[1];
+    
+    calcInput.length = 0;
 
     switch(operator) {
         case "+":
+            console.log(add(x,y));
+            return add(x,y);
             break;
         case "-":
+            console.log(subtract(x,y));
+            return subtract(x,y);
             break;
         case "×":
+            console.log(multiply(x,y));
+            return multiply(x,y);
             break;
         case "÷":
+            console.log(divide(x,y));
+            return divide(x,y);
             break;
     };
 };
