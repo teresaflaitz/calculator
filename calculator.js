ffb1af8b36e1197de2324206843af09e60c6239f
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-12 21:16:42 -0500

    Made function to create calculator buttons

--- calculator.js
+++ calculator.js
@@ -21,3 +21,26 @@ console.log(add(1,2));
 console.log(subtract(1,2));
 console.log(multiply(1,2));
 console.log(divide(1,2));
+
+function createButtons() {
+    const buttons = document.querySelector("#buttons");
+    const numbers = document.querySelector("#numbers");
+    const operators = document.querySelector("#operators");
+    const ops = ["+", "-", "×", "÷"];
+
+    for(let i = 0; i <= 9; i++) {
+        const button = document.createElement("button");
+        button.id = i;
+        button.textContent = i;
+        numbers.appendChild(button);
+    };
+
+    ops.forEach((i) => {
+        const button = document.createElement("button");
+        button.id = i;
+        button.textContent = i;
+        operators.appendChild(button);
+    });
+};
+
+createButtons();
